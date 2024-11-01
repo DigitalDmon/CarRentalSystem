231acf0f6ac433aeeac6aa03f32a097fc789b3bd
DigitalDmon <eladio.gonzalez@utp.ac.pa>
2024-11-01 02:35:42 -0500

    Prueba de conexión y consulta a la base de datos

---
+++ PresentationLayer/MainForm.vb
@@ -0,0 +1,15 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Instanciar la clase de conexión
+        Dim dbConnection As New DatabaseConnection()
+
+        ' Llamar al método ObtenerDatos con una consulta SQL
+        Dim query As String = "SELECT * FROM customers"
+        Dim dataTable As DataTable = dbConnection.ObtenerDatos(query)
+
+        ' Mostrar los datos en un DataGridView (por ejemplo)
+        DataGridView1.DataSource = dataTable
+
+    End Sub
+End Class
